0ebc3d6e7277405b15ddb3877c8621ced3eb662e
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-03-09 23:54:14 -0400

    Refact: se refactorizo calcular descuento por precio neto

---
+++ src/calcularDescuentoPorPrecioNeto.js
@@ -0,0 +1,22 @@
+function calcularDescuentoPorPrecioNeto(neto){
+    if (neto == 1000) {
+      return neto * 3.00 / 100;
+    }
+    else if(neto == 3000)
+    {
+      return neto * 5.00 / 100;
+    }else if(neto == 7000)
+    {
+      return neto * 7.00 / 100;
+    }else if(neto == 10000)
+    {
+      return neto * 10.00 / 100;
+    }else if(neto == 30000)
+    {
+      return neto * 15.00 / 100;
+    }
+    else {
+      return 0;
+    }
+  }
+  export default calcularDescuentoPorPrecioNeto;
--- src/totalizador.js
+++ src/totalizador.js
@@ -1,5 +1,6 @@
 import calcularPrecioNeto from "./calcularPrecioNeto.js";
 import calcularImpuestoPorEstado from "./calcularImpuestoPorEstado.js";
+import calcularDescuentoPorPrecioNeto from "./calcularDescuentoPorPrecioNeto.js"
 
 function totalizar(precio, cantidad, estado) {
   var neto = calcularPrecioNeto(precio, cantidad);
@@ -8,26 +9,5 @@ function totalizar(precio, cantidad, estado) {
 return neto - descuento + impuesto;
 }
 
-function calcularDescuentoPorPrecioNeto(neto){
-  if (neto == 1000) {
-    return neto * 3.00 / 100;
-  }
-  else if(neto == 3000)
-  {
-    return neto * 5.00 / 100;
-  }else if(neto == 7000)
-  {
-    return neto * 7.00 / 100;
-  }else if(neto == 10000)
-  {
-    return neto * 10.00 / 100;
-  }else if(neto == 30000)
-  {
-    return neto * 15.00 / 100;
-  }
-  else {
-    return 0;
-  }
-}
 
 export default totalizar;
