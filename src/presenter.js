a77cf999088b88b0205241d97c69267f34af7743
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-03-09 22:58:56 -0400

    fix: Se arreglo el interfaz de usuario para escoger el usuario

--- src/presenter.js
+++ src/presenter.js
@@ -4,6 +4,7 @@ import totalizar from "./totalizador.js";
  const second = document.querySelector("#cantidad");
  const form = document.querySelector("#totalizador-form");
  const div = document.querySelector("#resultado-div");
+ const impuesto = document.querySelector("#estado");
  
  form.addEventListener("submit", (event) => {
    event.preventDefault();
@@ -11,7 +12,7 @@ import totalizar from "./totalizador.js";
    
    const precio = Number.parseInt(first.value);
    const cantidad = Number.parseInt(second.value);
- 
+   const estado = impuesto.value;
    
-   div.innerHTML = "<p>" + totalizar(precio, cantidad) + "</p>";
+   div.innerHTML = "<p>" + calcular(precio, cantidad, estado) + "</p>";
  });
