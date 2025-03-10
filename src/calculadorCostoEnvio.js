7e99a5ea7c46c5eb122de8b0f2ea9941efa981a2
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-03-09 20:54:11 -0400

    feat: se agrego costo de envio por peso de 10-20

--- src/calculadorCostoEnvio.js
+++ src/calculadorCostoEnvio.js
@@ -1,6 +1,8 @@
 function calculadorCostoEnvio(peso) {
     if(peso > 0 && peso <= 10){
         return 0;
+    }else if (peso > 10 && peso <= 20){
+        return 3.5;
     }
 }
 
--- src/calculadorCostoEnvio.test.js
+++ src/calculadorCostoEnvio.test.js
@@ -4,4 +4,7 @@ import calcularCostoEnvio from './calculadorCostoEnvio';
      it('deberia calcular el costo de envio de un paquete de 1kg', () => {
          expect(calcularCostoEnvio(1)).toEqual(0);
      });
+     it('deberia calcular el costo de envio de un paquete de 11kg', () => {
+        expect(calcularCostoEnvio(11)).toEqual(3.5);
+    });
  });
