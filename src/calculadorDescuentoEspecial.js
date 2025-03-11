1b2c2c570ff979231e59be9655e5bf2390cef02f
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-03-10 23:28:13 -0400

    feat: se agrego el descuento especial de 200$

--- src/calculadorDescuentoEspecial.js
+++ src/calculadorDescuentoEspecial.js
@@ -3,6 +3,9 @@ function calcularDescuentoEspecial(neto, categoria, cliente)
      if(neto > 3000 && categoria === "Alimentos" && cliente === "recurrente"){
          return 100;
      }
+     else if (neto > 7000 && categoria === "Electronicos" && cliente === "especial"){
+        return 200;
+    }
  }
  
  export default calcularDescuentoEspecial;
--- src/calculadorDescuentoEspecial.test.js
+++ src/calculadorDescuentoEspecial.test.js
@@ -4,4 +4,7 @@ import calcularDescuentoEspecial from './calculadorDescuentoEspecial';
      it('debería retornar 100 si el neto es mayor a 3000, la categoria es Alimentos y el cliente es recurrente', function() {
          expect(calcularDescuentoEspecial(4000, "Alimentos", "recurrente")).toEqual(100);
      });
+     it('debería retornar 200 si el neto es mayor a 7000, la categoria es Electronicos y el cliente es especial', function() {
+        expect(calcularDescuentoEspecial(8000, "Electronicos", "especial")).toEqual(200);
+    });
  });
