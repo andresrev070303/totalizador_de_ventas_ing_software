72e2a9df84fd87cf4cc48381868bb4177959b5fc
andresrev070303 <andres.revollo@ucb.edu.bo>
2025-03-10 22:57:22 -0400

    feat: se agrego el descuento de envio por cliente recurrente

--- src/calculadorDescuentoEnvio.js
+++ src/calculadorDescuentoEnvio.js
@@ -2,5 +2,8 @@ function calcularDescuentoEnvio(precio, tipoCliente){
     if(tipoCliente === "normal"){
         return 0;
     }
+    else if (tipoCliente === "recurrente"){
+        return 0.5;
+    }
 }
 export default calcularDescuentoEnvio;
--- src/calculadorDescuentoEnvio.test.js
+++ src/calculadorDescuentoEnvio.test.js
@@ -4,4 +4,7 @@ import calcularDescuentoEnvio from "./calculadorDescuentoEnvio";
      it("deberia calcular el descuento de envío de un cliente normal", () => {
          expect(calcularDescuentoEnvio(100, "normal")).toEqual(0);
      });
+     it("deberia calcular el descuento de envío de un cliente recurrente", () => {
+        expect(calcularDescuentoEnvio(100, "recurrente")).toEqual(0.5);
+    });
  });
